da32196c629d8abc2d9a6a97303dc1ee9530cf96
Abiram <uthayakumarabiram@gmail.com>
2024-03-24 04:49:20 +0530

    hope1

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,4 +1,4 @@
-import { legacy_createStore, combineReducers, applyMiddleware } from "redux";
+import { createStore, combineReducers, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
 import {
@@ -59,7 +59,7 @@ const initialState = {
 
 
 
-const store = legacy_createStore(
+const store = createStore(
   reducer,
   initialState,
   composeWithDevTools(applyMiddleware(thunk))
