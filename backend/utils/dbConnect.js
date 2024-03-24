faee083b59fc909c3fff1910bd75080c00c78ca5
Abiram <uthayakumarabiram@gmail.com>
2024-03-24 06:19:15 +0530

    hope14

--- backend/utils/dbConnect.js
+++ backend/utils/dbConnect.js
@@ -1,16 +1,18 @@
 import mongoose from "mongoose";
 
-const dbConnect = () => {
+const dbConnect = async () => {
   try {
-    const mongoString = process.env.DATABASE_URL
-    mongoose.connect(mongoString);
-    const database = mongoose.connection
-    
-     database.once('connected', () => {
+    const mongoString = process.env.DATABASE_URL;
+    await mongoose.connect(mongoString, {
+      useCreateIndex: true,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      dbName: process.env.DATABASE_NAME // You might want to use a separate environment variable for the database name
+    });
     console.log('Database Connected');
-})
   } catch (error) {
-    console.log("DAtabase error");
+    console.error("Database connection error:", error);
   }
 };
+
 export default dbConnect;
