2b9385a0da82aa97b32a2cd58fc2e62d375de6a5
Abiram <uthayakumarabiram@gmail.com>
2024-03-25 06:28:14 +0530

    t27

--- backend/utils/dbConnect.js
+++ backend/utils/dbConnect.js
@@ -3,7 +3,11 @@ const mongoose = require('mongoose');
 const dbConnect = async () => {
     try {
         const mongoString = process.env.MONGODB_URI;
-        await mongoose.connect(mongoString, );
+        await mongoose.connect(mongoString,{
+            useUnifiedTopology: true,
+            useNewUrlParser: true,
+            serverSelectionTimeoutMS: 3000, // Set timeout to 3 seconds
+        } );
         console.log('Database Connected');
     } catch (error) {
         console.error("Database connection error:", error);
