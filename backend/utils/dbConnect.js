5b3a84e4d125755aefd24db5b3976086b3e0f7ea
Abiram <uthayakumarabiram@gmail.com>
2024-03-25 06:19:57 +0530

    t22

--- backend/utils/dbConnect.js
+++ backend/utils/dbConnect.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const dbConnect = async () => {
     try {
-        const mongoString = process.env.DATABASE_URL;
+        const mongoString = process.env.MONGODB_URI;
         await mongoose.connect(mongoString, );
         console.log('Database Connected');
     } catch (error) {
